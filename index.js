6015175b3afacb0d7023c4426ec4217ac565df21
Tom <emailfortomg@gmail.com>
2020-02-06 18:19:33 +0000

    Reconfigured Dragon so it works as a command line tool

---
+++ index.js
@@ -0,0 +1,14 @@
+const Dragon = require("./src/dragon.js");
+
+const main = function() {
+  let args = process.argv;
+
+  const dragon = new Dragon();
+  if (args.length === 2) {
+    dragon.runPrompt();
+  } else {
+    dragon.runfile(args[2]);
+  }
+};
+
+main();
