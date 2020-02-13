108b80c4bb781828671c90747f6fb60609ae8ff0
Tom <emailfortomg@gmail.com>
2020-02-13 17:19:10 +0000

    Removed clock() global function

--- src/lib/globalLib.js
+++ src/lib/globalLib.js
@@ -6,13 +6,6 @@ const Expr = require("../Expr.js");
  */
 module.exports = function(globals) {
   globals.defineVar(
-    "clock",
-    new StandardFn(0, function() {
-      return Date.now() / 1000;
-    })
-  );
-
-  globals.defineVar(
     "len",
     new StandardFn(1, function(obj) {
       return obj.length;
