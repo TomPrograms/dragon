4eda42dd3bbb0569724b4e6b589058134ac42213
Tom <emailfortomg@gmail.com>
2020-02-16 12:54:29 +0000

    Ensured only right data types provided to len()

--- src/lib/globalLib.js
+++ src/lib/globalLib.js
@@ -1,5 +1,8 @@
 const RuntimeError = require("../errors.js").RuntimeError;
+const DragonFunction = require("../structures/function.js");
+const DragonInstance = require("../structures/instance.js");
 const StandardFn = require("../structures/standardFn.js");
+const DragonClass = require("../structures/class.js");
 
 /**
  * Load all standard global functions to interpreter.
@@ -8,6 +11,41 @@ module.exports = function(globals) {
   globals.defineVar(
     "len",
     new StandardFn(1, function(obj) {
+      // don't allow len() to be called for numbers
+      if (!isNaN(obj)) {
+        throw new RuntimeError(
+          this.token,
+          "You can not find the length of a number."
+        );
+      }
+
+      // don't allow len() to be called for instances
+      if (obj instanceof DragonInstance) {
+        throw new RuntimeError(
+          this.token,
+          "You can not find the length of an instance."
+        );
+      }
+
+      if (obj instanceof DragonFunction) {
+        return obj.declaration.params.length;
+      }
+
+      if (obj instanceof StandardFn) {
+        return obj.arityValue;
+      }
+
+      if (obj instanceof DragonClass) {
+        let methods = obj.methods;
+        let length = 0;
+
+        if (methods.init && methods.init.isInitializer) {
+          length = methods.init.declaration.params.length;
+        }
+
+        return length;
+      }
+
       return obj.length;
     })
   );
