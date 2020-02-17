a388de42e0ce47479bad12ecf5a7743f08171a28
Tom <emailfortomg@gmail.com>
2020-02-17 19:12:01 +0000

    Added time.sleep() command

--- src/lib/time.js
+++ src/lib/time.js
@@ -6,3 +6,9 @@ module.exports.localTime = function(timestamp) {
   let timeFormatted = timestamp !== null ? new Date(timestamp) : new Date();
   return timeFormatted;
 };
+
+module.exports.sleep = function(ms) {
+  let now = new Date().getTime();
+  while (new Date().getTime() < now + ms) {}
+  return null;
+}
